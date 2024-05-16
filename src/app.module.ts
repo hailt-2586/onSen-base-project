26759cdb2be569ab230bef37272c0915eac45350
Le Hai <le.thanh.hai@sun-asterisk.com>
2024-05-16 14:05:57 +0700

    Configuration env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { envConfig } from '@config/env.config';
+import { NODE_ENV_DEV } from '@shared/constants/common.constant';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === NODE_ENV_DEV ? '.env' : undefined,
+      load: [envConfig],
+    }),
+  ],
 })
 export class AppModule {}
---
+++ src/shared/constants/common.constant.ts
@@ -0,0 +1,2 @@
+export const NODE_ENV_PRD: string = 'production';
+export const NODE_ENV_DEV: string = 'development';
